780223e021c410e7f9dd9fcb2da367b3493bea61
Evan <ekprogamer13@gmail.com>
2022-04-10 11:24:13 -0400

    added some code, need to sort out all the bad apples that don't work

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver import ChromeOptions
+# -------------------------------------------------------------------------------------------------------------------
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from fake_useragent import UserAgent
 from selenium_stealth import stealth
 from bs4 import BeautifulSoup
 import os
-
-os.environ['WDM_LOG_LEVEL'] = '0'    # prevents webdriver manager from printing, it is quite annoying tbh
-
+os.environ['WDM_LOG_LEVEL'] = '0'
 ua = UserAgent(verify_ssl=False)
-
 options = ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_experimental_option('useAutomationExtension', False)
-options.add_argument('--disable-blink-features=AutomationControlled')    # helps go undetected
+options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument(f'--user-agent={ua}')
 options.add_argument('--window-size=960,540')
-options.add_argument('--incognito')    # doesn't save cookies after session
+options.add_argument('--incognito')
 driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-# adds extra protection against getting detected
 stealth(driver,
         languages=['en-US', 'en'],
         vendor='Google Inc.',
@@ -29,8 +24,10 @@ stealth(driver,
         webgl_vendor='Intel Inc.',
         renderer='Intel Iris OpenGL Engine',
         fix_hairline=True)
-
 driver.set_window_position(0, 0, windowHandle='current')
+# -------------------------------------------------------------------------------------------------------------------
+
+
 
 data = []
 total = ''
@@ -43,11 +40,23 @@ for i in soup.find('p', {'class': 'header__description'}).get_text().strip():
         total = total + i
 total = int(total)
 
-print(f'\n{total} icon names to grab!')
-print('Okay! I am going to iterate through all of them and export the data to a file. This data will work with .md file types!\n')
-
-for i in range(total):
-    data.append(soup.find('li', {'order-alpha': i}).get_text())
-    payload = f'{data[i]}' + ' '*(50-len(data[i])) + f'{i+1}/{total}' + ' '*(10-len(str(f'{i+1}/{total}'))) + ' completed' + ' '*(2) + '|' + ' '*(2) + f'{round((i/(total-1)*100), 10)}%' + ' '*(15-len(str(round((i/(total-1)*100), 10)))) + ' done'
+for index, tag in enumerate(soup.find_all('h2', {'class': 'grid-item__title'})):
+    data.append(tag.get_text())
+    payload = f'{data[index]}' + ' '*(40-len(data[index])) + f'{index+1}/{total}' + ' '*(10-len(str(f'{index+1}/{total}'))) + ' completed' + ' '*(2) + '|' + ' '*(2) + f'{round((index/(total-1)*100), 10)}%' + ' '*(15-len(str(round((index/(total-1)*100), 10)))) + ' done'
     print(payload)
+
+with open('names.txt', 'w') as file:
+    for i in data:
+        i.replace(' ', '-')
+        driver.get(f'https://img.shields.io/static/v1?style=for-the-badge&logo={i}&label=&message=­&color=0d1116')
+
+
+
+
+
+
+
+
+
+# file.write(f'![](https://img.shields.io/static/v1?style=for-the-badge&logo={i}&label=&message=­&color=0d1116)\n')
 
